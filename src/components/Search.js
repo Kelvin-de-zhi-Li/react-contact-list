0d333307192849302c4737a900b7f0dad4331cf9
Kelvin-de-zhi-Li <kaungkelvin168@gmail.com>
2021-08-27 19:18:00 +0630

    changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,13 @@ import React from "react";
 const Search = ({ onChange }) => {
   return (
     <div className="form-group row">
-      <input
-        onChange={onChange}
-        className="form-control"
-        placeholder="Search Contact"
-      ></input>
+      <div className="col-xs-4">
+        <input
+          onChange={onChange}
+          className="form-control"
+          placeholder="Search Contact"
+        ></input>
+      </div>
     </div>
   );
 };
