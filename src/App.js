8d7e421623c0645fce0e15423e3383a09546c8eb
Kelvin-de-zhi-Li <kaungkelvin168@gmail.com>
2021-08-30 13:31:29 +0630

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Contacts from "./components/Contacts";
 import AddContact from "./components/AddContact";
 import Search from "./components/Search";
